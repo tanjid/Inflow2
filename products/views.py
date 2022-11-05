73b43f51b9efb18c5fa172b7c748e31c366e3fec
tanjid <tanjid.du@gmail.com>
2022-11-05 14:45:39 +0600

    adad

--- products/views.py
+++ products/views.py
@@ -112,32 +112,35 @@ class LowStockView(TemplateView):
     #                 name=row['name'],
     #                 cat_id = row["id"]
     #             )
-    with open('products/product.csv', 'r') as csvfile:
-        read_csv = csv.DictReader(csvfile)
+    # with open('products/product2.csv', 'r') as csvfile:
+    #     read_csv = csv.DictReader(csvfile)
 
-        for row in read_csv:
-            print(f"{row['sku']} ")
+    #     for row in read_csv:
+    #         print(f"{row['sku']} ")
 
-            if Product.objects.filter(sku=row['sku']).exists():
-                pass
+    #         if Product.objects.filter(sku=row['sku']).exists():
+    #             pass
                 
-            else:
-                image_url = f"products/{row['product_image']}"
-                brand = ProductBrand.objects.get(brand_id=row['brand_id'])
-                cat = ProductCategory.objects.get(cat_id=row['category_id'])
-                Product.objects.create(
-                    name=row['name'],
-                    sku=row['sku'],
-                    brand=brand,
-                    category=cat,
-                    warranty=row['warranty'],
-                    warranty_policy=row['warranty_policy'],
-                    cost_price=int(row['price'].replace(',','')),
-                    sell_price=int(row['sell_price'].replace(',','')),
-                    alert_qty=2,
-                    stock_qty=10,
-                    product_image=image_url,
-                )
+    #         else:
+    #             image_url = f"products/{row['product_image']}"
+    #             brand = ProductBrand.objects.get(brand_id=row['brand_id'])
+    #             try:
+    #                 cat = ProductCategory.objects.get(cat_id=row['category_id'])
+    #             except:
+    #                 cat = None
+    #             Product.objects.create(
+    #                 name=row['name'],
+    #                 sku=row['sku'],
+    #                 brand=brand,
+    #                 category=cat,
+    #                 warranty=row['warranty'],
+    #                 warranty_policy=row['warranty_policy'],
+    #                 cost_price=int(row['price'].replace(',','')),
+    #                 sell_price=int(row['sell_price'].replace(',','')),
+    #                 alert_qty=2,
+    #                 stock_qty=10,
+    #                 product_image=image_url,
+    #             )
 
 
     template_name = 'products/low_stock_notification.html'
